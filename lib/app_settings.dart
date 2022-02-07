ccd6bb0ada922fcf34b78d928f5c5e89b0005f79
BigDeeEss <damon.swatton@gmail.com>
2022-02-07 20:45:18 +0000

    Add button.dart.

--- lib/app_settings.dart
+++ lib/app_settings.dart
@@ -7,57 +7,57 @@ class AppSettings {
   // /// appBar when calculating bottomNavigationBar height in BasePage class.
   // static double appBarHeightScaleFactor = 1.0;
   //
-  // /// [buttonAlignment] defines the anchor point for button placement
-  // /// in ButtonArray class.
-  // // static Alignment buttonAlignment = Alignment.bottomLeft;
-  // // static Alignment buttonAlignment = Alignment.bottomRight;
-  // static Alignment buttonAlignment = Alignment.topLeft;
-  // // static Alignment buttonAlignment = Alignment.topRight;
-  //
-  // /// [buttonAxis] sets the button axis in ButtonArray.
-  // // static Axis buttonAxis = Axis.horizontal;
-  // static Axis buttonAxis = Axis.vertical;
-  //
+  /// [buttonAlignment] defines the anchor point for button placement
+  /// in ButtonArray class.
+  // static Alignment buttonAlignment = Alignment.bottomLeft;
+  // static Alignment buttonAlignment = Alignment.bottomRight;
+  static Alignment buttonAlignment = Alignment.topLeft;
+  // static Alignment buttonAlignment = Alignment.topRight;
+
+  /// [buttonAxis] sets the button axis in ButtonArray.
+  // static Axis buttonAxis = Axis.horizontal;
+  static Axis buttonAxis = Axis.vertical;
+
   // /// [buttonAnimationTime] defines the time in milliseconds allowed
   // /// for the button animation in BasePage.
   // static int buttonAnimationTime = 1500;
-  //
-  // /// [buttonPaddingDown] defines the padding for downward direction
-  // /// button positioning in Button class.
-  // static EdgeInsetsDirectional buttonPaddingDown =
-  //     EdgeInsetsDirectional.fromSTEB(
-  //   buttonPaddingCrossAxis,
-  //   buttonPaddingMainAxis,
-  //   buttonPaddingCrossAxis,
-  //   buttonPaddingMainAxisExtra,
-  // );
-  //
-  // /// [buttonPaddingCrossAxis] defines the cross axis padding between buttons.
-  // static double buttonPaddingCrossAxis = 15.0;
-  //
-  // /// [buttonPaddingMainAxis] defines the main axis padding between buttons.
-  // static double buttonPaddingMainAxis = 15.0;
-  //
-  // /// [buttonPaddingMainAxisExtra] defines extra padding in main axis direction.
-  // static double buttonPaddingMainAxisExtra = 10.0;
-  //
-  // /// [buttonPaddingUp] defines the padding for downward direction
-  // /// button positioning in Button class.
-  // static EdgeInsetsDirectional buttonPaddingUp =
-  //     EdgeInsetsDirectional.fromSTEB(
-  //   buttonPaddingCrossAxis,
-  //   buttonPaddingMainAxisExtra,
-  //   buttonPaddingCrossAxis,
-  //   buttonPaddingMainAxis,
-  // );
+
+  /// [buttonPaddingDown] defines the padding for downward direction
+  /// button positioning in Button class.
+  static EdgeInsetsDirectional buttonPaddingDown =
+      EdgeInsetsDirectional.fromSTEB(
+    buttonPaddingCrossAxis,
+    buttonPaddingMainAxis,
+    buttonPaddingCrossAxis,
+    buttonPaddingMainAxisExtra,
+  );
+
+  /// [buttonPaddingCrossAxis] defines the cross axis padding between buttons.
+  static double buttonPaddingCrossAxis = 15.0;
+
+  /// [buttonPaddingMainAxis] defines the main axis padding between buttons.
+  static double buttonPaddingMainAxis = 15.0;
+
+  /// [buttonPaddingMainAxisExtra] defines extra padding in main axis direction.
+  static double buttonPaddingMainAxisExtra = 10.0;
+
+  /// [buttonPaddingUp] defines the padding for downward direction
+  /// button positioning in Button class.
+  static EdgeInsetsDirectional buttonPaddingUp =
+      EdgeInsetsDirectional.fromSTEB(
+    buttonPaddingCrossAxis,
+    buttonPaddingMainAxisExtra,
+    buttonPaddingCrossAxis,
+    buttonPaddingMainAxis,
+  );
 
   /// [buttonSize] defines the button size in Button class.
   static double buttonSize = 56.0;
 
-  // /// [drawLayoutBounds] triggers whether layout bounds are draw or not.
-  // /// Used for debugging widget screen location.
-  // static bool drawLayoutBounds = true;
-  //
+  /// [drawLayoutBounds] triggers whether layout bounds are draw or not.
+  /// Used for debugging widget screen location.
+  static bool drawLayoutBounds = true;
+
   // /// [pageTransitionTime] defines the time in milliseconds allowed
   // /// for the transitioning the page.
   // static int pageTransitionTime = 750;
