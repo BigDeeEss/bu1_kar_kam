f1549770802a371d914aef913dffe8c9b9de32b8
BigDeeEss <damon.swatton@gmail.com>
2022-02-07 16:21:06 +0000

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+//  Import flutter packages.
+import 'package:flutter/material.dart';
+
+//  Import project-specific files.
+// import 'package:dash_cam_app/base_page.dart';
+// import 'package:dash_cam_app/page_specs.dart';
+
+//  App start point.
+void main() {
+  runApp(const _KarKam());
+}
+
+/// [_KarKam] is the root widget of this project.
+class _KarKam extends StatelessWidget {
+  const _KarKam({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '_KarKam',
+
+      //  BasePage invokes a generic page layout: a similar UI is
+      //  presented for each page(route).
+      home: Container(),
+      // home: BasePage(
+      //   pageSpec: settingsPage,
+      // ),
+    );
+  }
+}
