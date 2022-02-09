48edec4efead199c331051ffc48e109300bd738c
BigDeeEss <damon.swatton@gmail.com>
2022-02-09 22:30:11 +0000

    Add settings_page_contents.dart.
    The associated class, SettingsPageContents, at this stage is only an empty stateless widget.

---
+++ lib/settings_page_contents.dart
@@ -0,0 +1,13 @@
+//  Import flutter packages.
+import 'package:flutter/material.dart';
+
+//  Import project-specific files.
+
+class SettingsPageContents extends StatelessWidget {
+  const SettingsPageContents({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
