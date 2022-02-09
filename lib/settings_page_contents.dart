cd08f379dce1396a5ad5b2bf6506f8d614a7f2ae
BigDeeEss <damon.swatton@gmail.com>
2022-02-09 22:43:24 +0000

    Add in a simple ListView for testing.

--- lib/settings_page_contents.dart
+++ lib/settings_page_contents.dart
@@ -3,11 +3,369 @@ import 'package:flutter/material.dart';
 
 //  Import project-specific files.
 
+/// [SettingsPageContents] implements a bespoke settings page by calling
+/// [_SettingsPageContentsList]. This introduces an additional layer in the
+/// widget tree for an instance of NotificationNotifier.
 class SettingsPageContents extends StatelessWidget {
   const SettingsPageContents({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return _SettingsPageContentsList();
+  }
+}
+
+class _SettingsPageContentsList extends StatelessWidget {
+  const _SettingsPageContentsList({Key? key}) : super(key: key);
+
+  final List<Color> colors = const [
+    Colors.blueGrey,
+    Colors.green,
+    Colors.deepOrange,
+    Colors.purple
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Card(
+          child: ListTile(
+            title: Text("Codesinsider.com"),
+          ),
+          elevation: 8,
+          shadowColor: Colors.green,
+          shape: BeveledRectangleBorder(
+              borderRadius: BorderRadius.circular(15)
+          ),
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        ListTile(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          selected: true,
+          selectedTileColor: Colors.grey[300],
+          leading: FlutterLogo(),
+          title: Text('ListTile'),
+        ),
+        Card(
+          child: ListTile(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+            selected: true,
+            selectedTileColor: Colors.grey[300],
+            leading: FlutterLogo(),
+            title: Text('ListTile'),
+            subtitle: SizedBox(height: 77,),
+          ),
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Card(
+          child: Container(
+            height: 50,
+            width: 50,
+            alignment: Alignment.center,
+            color: colors[2],
+            child: Text('2'),
+          ),
+        ),
+        Card(
+          child: Container(
+            height: 50,
+            width: 50,
+            alignment: Alignment.center,
+            color: colors[2],
+            child: Text('1'),
+          ),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Card(
+          child: Container(
+            height: 50,
+            width: 50,
+            alignment: Alignment.center,
+            color: colors[2],
+          ),
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[1],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[2],
+        ),
+        Container(
+          height: 50,
+          width: 50,
+          alignment: Alignment.center,
+          color: colors[0],
+        ),
+      ],
+    );
   }
 }
