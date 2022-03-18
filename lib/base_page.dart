72d7d3420ec591446f6d4295db651eb40f500bc0
Damon Swatton on PAVILION <damon.swatton@gmail.com>
2022-03-18 16:10:24 +0000

    Make BasePage a StatefulWidget.
    Initial build of BasePage only includes ButtonArray. A new variable, pageContents, is either null or widget.pageSpec.contents (after setState).

--- lib/base_page.dart
+++ lib/base_page.dart
@@ -10,7 +10,7 @@ import 'package:kar_kam/page_specs.dart';
 
 /// [BasePage] implements a generic page layout design so that a
 /// similar UI is presented for each page/route.
-class BasePage extends StatelessWidget {
+class BasePage extends StatefulWidget {
   BasePage({
     Key? key,
     required this.pageSpec,
@@ -19,16 +19,37 @@ class BasePage extends StatelessWidget {
   /// [pageSpec] defines the page content.
   final PageSpec pageSpec;
 
+  @override
+  State<BasePage> createState() => _BasePageState();
+}
+
+class _BasePageState extends State<BasePage> {
   /// [buttonArrayRectData] stores Rect information that is dispatched as
   /// notifications by ButtonArray.
   final ValueNotifier<Rect?> buttonArrayRectData
       = ValueNotifier(Offset(0.0, 0.0) & Size(0.0, 0.0));
 
+  /// [pageContents] (initially null) is updated by setState in a
+  /// postFrameCallback.
+  Widget? pageContents;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      if (pageContents == null) {
+        setState(() {
+          pageContents = widget.pageSpec.contents;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(pageSpec.title),
+        title: Text(widget.pageSpec.title),
       ),
       //  Use Builder widget because it is not possible to get the appBar
       //  height from the current BuildContext when it doesn't yet include
@@ -64,10 +85,16 @@ class BasePage extends StatelessWidget {
         //  Place page contents and ButtonArray on screen using Stack.
         //  Ensure that ButtonArray sits above the page content by placing
         //  it last in a Stack list of children.
+        //
+        //  If [pageContents] is null then post an empty container into Stack,
+        //  otherwise use its value.
+        //
+        //  Note: [pageContents] equates to widget.pageSpec.contents
+        //  after setState.
         child: Stack(
           children: <Widget>[
+            pageContents ?? Container(),
             ButtonArray(),
-            pageSpec.contents,
           ],
         ),
       ),
