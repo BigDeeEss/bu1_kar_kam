4e02bb147c3cc9bc38ddd98eb31393cf371e1bdc
Damon Swatton on ENVY <damon.swatton@gmail.com>
2022-03-24 20:26:05 +0000

    Delete unnecessary getRectAbsolute and all references to buttonListGlobalKey in ButtonArray.

--- lib/button_array.dart
+++ lib/button_array.dart
@@ -2,12 +2,9 @@
 import 'package:flutter/material.dart';
 
 // Import project-specific files.
-// import 'package:kar_kam/animation_status_notification.dart';
 import 'package:kar_kam/app_settings.dart';
 import 'package:kar_kam/button.dart';
 import 'package:kar_kam/button_specs.dart';
-import 'package:kar_kam/lib/data_notification.dart';
-import 'package:kar_kam/lib/global_key_extension.dart';
 
 /// [ButtonArray] implements a linear horizontal or vertical button array
 /// in any of the four screen corners.
@@ -21,14 +18,6 @@ class ButtonArray extends StatelessWidget {
     homeButton,
   ];
 
-  /// [buttonListGlobalKey] is an array of GlobalKeys that enable Rect
-  /// data from each button to be obtained using the GlobalKeyExtension
-  /// method, globalPaintBounds.
-  //
-  //  The length of this list is determined by buttonSpecList via
-  //  class method buttonList.
-  final buttonListGlobalKey = <GlobalKey>[];
-
   /// [buttonList] generates a list of buttons from buttonSpecList.
   List<Widget> buttonList() {
     //  Initialise widgetList so that it is ready for population.
@@ -37,8 +26,6 @@ class ButtonArray extends StatelessWidget {
     //  Loop over items in buttonSpecList and convert each to its
     //  corresponding button.
     for (int i = 0; i < buttonSpecList.length; i++) {
-      buttonListGlobalKey.add(GlobalKey());
-
       //  Treat horizontal and vertical axes differently.
       if (AppSettings.buttonAxis == Axis.horizontal) {
         //  The top/bottom inputs to Positioned must be either 0.0/null,
@@ -61,7 +48,6 @@ class ButtonArray extends StatelessWidget {
               : null,
           child: Button(
             buttonSpec: buttonSpecList[i],
-            key: buttonListGlobalKey[i],
           ),
         ));
       }
@@ -87,7 +73,6 @@ class ButtonArray extends StatelessWidget {
           right: (AppSettings.buttonAlignment.x > 0) ? 0.0 : null,
           child: Button(
             buttonSpec: buttonSpecList[i],
-            key: buttonListGlobalKey[i],
           ),
         ));
       }
@@ -95,35 +80,8 @@ class ButtonArray extends StatelessWidget {
     return widgetList;
   }
 
-  Rect? getRectAbsolute() {
-    // Instantiate rect as null.
-    Rect? rect;
-    //  Loop over buttonListGlobalKey. buttonListGlobalKey has the same
-    //  length as buttonSpecList.
-    for (int i = 0; i < buttonListGlobalKey.length; i++) {
-      //  Get Rect data for ith button.
-      Rect? buttonRect = buttonListGlobalKey[i].globalPaintBounds;
-      if (buttonRect != null) {
-        //  If rect is null then overwrite with buttonRect, else expand
-        //  rect to include buttonRect.
-        if (rect == null) {
-          rect = buttonRect;
-        } else {
-          rect = rect.expandToInclude(buttonRect);
-        }
-      }
-    }
-    return rect;
-  }
-
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
-      Rect? buttonArrayRect = getRectAbsolute();
-      DataNotification(
-        data: buttonArrayRect,
-      ).dispatch(context);
-    });
     //  Return a Stack with a list of children defined by buttonList.
     //  Output from buttonList is a list of buttons of length equal to
     //  buttonSpecList.length.
