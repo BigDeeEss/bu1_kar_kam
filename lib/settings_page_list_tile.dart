73e42b690428de619a559c148d9dec3d94a70bb2
Damon Swatton on ENVY <damon.swatton@gmail.com>
2022-03-26 14:22:31 +0000

    Save.

--- lib/settings_page_list_tile.dart
+++ lib/settings_page_list_tile.dart
@@ -4,25 +4,36 @@ import 'package:kar_kam/lib/data_notifier.dart';
 import 'package:kar_kam/lib/global_key_extension.dart';
 
 class SettingsPageListTile extends StatelessWidget {
-  const SettingsPageListTile({Key? key}) : super(key: key);
+  SettingsPageListTile({Key? key}) : super(key: key);
 
-  final GlobalKey? buttonArrayGlobalKey;
-  final Rect? buttonArrayRect;
+  // late GlobalKey? buttonArrayGlobalKey;
+  // late Rect? buttonArrayRect;
 
   @override
   Widget build(BuildContext context) {
-    GlobalKey? buttonArrayGlobalKey = DataNotifier
+    Rect? buttonArrayRect;
+    //  Attempt to get buttonArrayGlobalKey (see base_page.dart).
+    GlobalKey buttonArrayGlobalKey = DataNotifier
         .of(context, ValueKey('buttonArrayGlobalKey')).data.value;
-    Rect? buttonArrayRect = buttonArrayGlobalKey?.globalPaintBounds;
     // print(buttonArrayGlobalKey);
-    // print(DataNotifier.of(context, ValueKey('buttonArrayGlobalKey')).data.value);
     // print(DataNotifier.of(context, ValueKey('scrollController')).data.value);
-    return ValueListenableBuilder<double>(
-      valueListenable: DataNotifier
-          .of(context, ValueKey('scrollController')).data,
-      builder: (BuildContext context, double value, __) {
-        return Card();
-      });
+    print(DataNotifier.of(context, ValueKey('buttonArrayGlobalKey')).data);
+    // Rect? buttonArrayRect = buttonArrayGlobalKey?.globalPaintBounds;
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      buttonArrayRect = buttonArrayGlobalKey.globalPaintBounds;
+      print(buttonArrayRect);
+    });
+    if (buttonArrayRect != null) {
+      return ValueListenableBuilder<double>(
+          valueListenable: DataNotifier
+              .of(context, ValueKey('scrollController'))
+              .data,
+          builder: (BuildContext context, double value, __) {
+            return Card();
+          });
+    } else {
+      return Container();
+    }
   }
 }
 
