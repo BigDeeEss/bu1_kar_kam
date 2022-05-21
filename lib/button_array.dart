976355192edae2e519cc8eda9475431295c544ac
Damon Swatton on PAVILION <damon.swatton@gmail.com>
2022-05-21 13:37:02 +0100

    General tidy up.
    Review comments and rename a few variables in ButtonArray.
    
    The original intention was to implement a way of positioning buttons at the centre of the screen at the edges, but this was proving to be too bulky, codewise.

--- lib/button_array.dart
+++ lib/button_array.dart
@@ -19,98 +19,81 @@ class ButtonArray extends StatelessWidget {
     homeButton,
   ];
 
-  /// [buttonArrayGlobalKeys] is an array of GlobalKeys that enable Rect data
-  /// from each button to be obtained using the GlobalKeyExtension method,
-  /// globalPaintBounds. This feature is implemented below in [getRect].
-  //
-  //  The length of this list is determined by [buttonSpecList] via
-  //  class method [buttonArrayGenerator].
+  /// [buttonArrayGlobalKeys] is a list of GlobalKeys that enable Rect data
+  /// for each button to be obtained using globalPaintBounds (see [getRect]).
   final buttonArrayGlobalKeys = <GlobalKey>[];
+
+  List<double> get buttonCoords {
+    //  Initialise [coordsList] so that it is ready for population.
+    List<double> coordsList = [];
+
+    //  Define a length value: button width plus padding.
+    double dim = (AppSettings.buttonRadiusInner +
+        AppSettings.buttonPaddingMainAxisExtra) * 2;
+
+    //  Loop over items in [buttonSpecList] and convert each to its
+    //  corresponding position.
+    for (int i = 0; i < buttonSpecList.length; i++) {
+      coordsList.add(dim * i);
+    }
+    return coordsList;
+  }
 
   /// [buttonArrayGenerator] generates a list of buttons from buttonSpecList.
   List<Widget> buttonArrayGenerator(BuildContext context) {
-    //  Initialise widgetList so that it is ready for population.
-    List<Widget> widgetList = [];
+    //  Initialise [button] and [buttonList] ready for population.
+    Button? button;
+    List<Widget> buttonList = [];
+
+    //  Take local copy of [buttonCoords] for speed.
+    List<double> coords = buttonCoords;
 
     //  Loop over items in [buttonSpecList] and convert each to its
     //  corresponding button.
     for (int i = 0; i < buttonSpecList.length; i++) {
       buttonArrayGlobalKeys.add(GlobalKey());
 
+      //  Define the buttonto be added to [buttonList] in this iteration.
+      button = Button(
+        buttonSpec: buttonSpecList[i],
+        key: buttonArrayGlobalKeys[i],
+      );
+
       //  Treat horizontal and vertical axes differently.
       if (AppSettings.buttonAxis == Axis.horizontal) {
-        //  The top/bottom inputs to Positioned must be either 0.0/null,
-        //  depending on whether selected alignment is top or bottom, in which
-        //  case it is the reverse configuration.
+        //  The top and bottom inputs to Positioned must be 0.0 or null,
+        //  depending on whether selected alignment is top or bottom
         //
-        //  The left/right inputs to Positioned must be non-zero
-        //  coordinates/null, depending on whether selected alignment is
-        //  left or right, in which case it is the reverse configuration.
-        widgetList.add(Positioned(
+        //  The left and right inputs to Positioned must be non-zero
+        //  coordinates or null, depending on whether selected alignment is
+        //  left or right.
+        buttonList.add(Positioned(
           top: (AppSettings.buttonAlignment.y < 0) ? 0 : null,
           bottom: (AppSettings.buttonAlignment.y > 0) ? 0 : null,
-          left: (AppSettings.buttonAlignment.x < 0)
-              ? (AppSettings.buttonRadiusInner +
-              AppSettings.buttonPaddingMainAxisExtra) * 2 * i
-              : null,
-          right: (AppSettings.buttonAlignment.x > 0)
-              ? (AppSettings.buttonRadiusInner +
-              AppSettings.buttonPaddingMainAxisExtra) * 2 * i
-              : null,
-          child: Button(
-            buttonSpec: buttonSpecList[i],
-            key: buttonArrayGlobalKeys[i],
-          ),
+          left: (AppSettings.buttonAlignment.x < 0) ? coords[i] : null,
+          right: (AppSettings.buttonAlignment.x > 0) ? coords[i] : null,
+          child: button,
         ));
       }
 
       //  Treat horizontal and vertical axes differently.
       if (AppSettings.buttonAxis == Axis.vertical) {
-        //  The left/right inputs to Positioned must be either 0.0/null,
-        //  depending on whether selected alignment is left or right, in which
-        //  case it is the reverse configuration.
+        //  The left and right inputs to Positioned must be 0.0 or null,
+        //  depending on whether selected alignment is left or right.
         //
-        //  The top/bottom inputs to Positioned must be non-zero
-        //  coordinates/null, depending on whether selected alignment is
-        //  top or bottom, in which case it is the reverse configuration.
-        widgetList.add(Positioned(
-          top: (AppSettings.buttonAlignment.y < 0)
-              ? (AppSettings.buttonRadiusInner +
-                  AppSettings.buttonPaddingMainAxisExtra) * 2 * i
-              : null,
-          bottom: (AppSettings.buttonAlignment.y > 0)
-              ? (AppSettings.buttonRadiusInner +
-                  AppSettings.buttonPaddingMainAxisExtra) * 2 * i
-              : null,
+        //  The top and bottom inputs to Positioned must be non-zero
+        //  coordinates or null, depending on whether selected alignment is
+        //  top or bottom.
+        buttonList.add(Positioned(
+          top: (AppSettings.buttonAlignment.y < 0) ? coords[i] : null,
+          bottom: (AppSettings.buttonAlignment.y > 0) ? coords[i] : null,
           left: (AppSettings.buttonAlignment.x < 0) ? 0.0 : null,
           right: (AppSettings.buttonAlignment.x > 0) ? 0.0 : null,
-          child: Button(
-            buttonSpec: buttonSpecList[i],
-            key: buttonArrayGlobalKeys[i],
-          ),
+          child: button,
         ));
-
-        // Align tmp = Align(
-        //   alignment: AppSettings.buttonAlignment,
-        //   child: Button(
-        //     buttonSpec: buttonSpecList[i],
-        //     key: buttonArrayGlobalKeys[i],
-        //   ),
-        // );
-        // RenderBox box = tmp.createRenderObject(context) as RenderBox;
-        // // translation = box.getTransformTo(null).getTranslation();
-        // print(box.paintBounds);
-        //
-        // widgetList.add(Align(
-        //   alignment: AppSettings.buttonAlignment,
-        //   child: Button(
-        //     buttonSpec: buttonSpecList[i],
-        //     key: buttonArrayGlobalKeys[i],
-        //   ),
-        // ));
       }
     }
-    return widgetList;
+    return buttonList;
   }
 
   Rect getRect() {
@@ -136,7 +119,7 @@ class ButtonArray extends StatelessWidget {
       }
     }
     assert(rect != null,
-      'button_array.dart, getRect: error rect is null when it shouldn\'t be.'
+    'button_array.dart, getRect: error rect is null when it shouldn\'t be.'
     );
     return rect!;
   }
@@ -150,6 +133,7 @@ class ButtonArray extends StatelessWidget {
     //  list of buttons. [buttonArray] is generated by [buttonArrayGenerator]
     //  and has length equal to buttonSpecList.length.
     return Stack(
+      alignment: Alignment.bottomRight,
       children: buttonArray,
     );
   }
