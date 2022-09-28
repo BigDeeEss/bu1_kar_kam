1c827365e10831c9cc1ca5b897ad77e4890eb6ce
Damon Swatton on PAVILION <damon.swatton@gmail.com>
2022-09-28 18:34:09 +0100

    Save.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 //  Import flutter packages.
 import 'package:flutter/material.dart';
+import 'package:kar_kam/app_settings.dart';
 
 //  Import project-specific files.
 import 'package:kar_kam/base_page.dart';
+import 'package:kar_kam/lib/data_notifier.dart';
 import 'package:kar_kam/page_specs.dart';
 
 //  App start point.
 void main() {
-  runApp(const _KarKam());
+  runApp(_KarKam());
 }
 
 /// [_KarKam] is the root widget of this project.
@@ -15,18 +17,24 @@ void main() {
 /// [_KarKam] loads an instance of [BasePage]; content is determined
 /// by [pageSpec].
 class _KarKam extends StatelessWidget {
-  const _KarKam({Key? key}) : super(key: key);
+  _KarKam({Key? key}) : super(key: key);
+
+  final ValueNotifier<AppSettings> appSettings = ValueNotifier(AppSettings());
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '_KarKam',
-      //  BasePage invokes a generic page layout so that a similar UI is
-      //  presented for each page (route).
-      home: BasePage(
-        pageSpec: settingsPage,
-        // pageSpec: filesPage,
-      ),
+    return DataNotifier(
+      key: ValueKey('appSettings'),
+      data: appSettings,
+      child: MaterialApp(
+        title: '_KarKam',
+        //  BasePage invokes a generic page layout so that a similar UI is
+        //  presented for each page (route).
+        home: BasePage(
+          pageSpec: settingsPage,
+          // pageSpec: filesPage,
+        ),
+      )
     );
   }
 }
