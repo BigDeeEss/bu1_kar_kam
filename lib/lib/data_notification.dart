97fd3effcbb6888bbeb2f68898428946a3bac075
Damon Swatton on PAVILION <damon.swatton@gmail.com>
2022-11-16 17:14:22 +0000

    Make DataNotification typed.
    Need to amend all instances of DataNotification in main code.

--- lib/lib/data_notification.dart
+++ lib/lib/data_notification.dart
@@ -2,10 +2,10 @@
 import 'package:flutter/material.dart';
 
 /// [DataNotification] bubbles data of any sort up the widget tree.
-class DataNotification extends Notification {
-  DataNotification({
+class DataNotification<T> extends Notification {
+  const DataNotification({
     required this.data,
   });
 
-  var data;
+  final T data;
 }
