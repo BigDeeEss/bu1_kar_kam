ac3a4569616e35596cb54ac0514594985d5877aa
Da,on Swatton on INSPIRON <damon.swatton@gmail.com>
2023-03-09 14:34:42 +0000

    Update comments in boxed_container.dart.

--- lib/boxed_container.dart
+++ lib/boxed_container.dart
@@ -1,17 +1,16 @@
 //  Import flutter packages.
 import 'package:flutter/material.dart';
 import 'package:get_it_mixin/get_it_mixin.dart';
-import 'package:kar_kam/settings.dart';
 
 //  Import project-specific files.
-// import 'package:kar_kam/settings_service_one.dart';
+import 'package:kar_kam/settings.dart';
 import 'package:kar_kam/settings_service.dart';
 
-/// [BoxedContainer] implements a Container and prints its bounding box.
+/// Implements a [Container] and draws its bounding box.
 ///
-/// The [BoxedContainer] essentially calls an instance of Container with
+/// [BoxedContainer] essentially calls an instance of [Container] with
 /// a default decoration if one is not specifically given. [BoxedContainer]
-/// defaults to Container if AppSettings.drawLayoutBounds is false.
+/// defaults to [Container] if [Settings.drawLayoutBounds] is false.
 class BoxedContainer extends StatelessWidget with GetItMixin {
   BoxedContainer({
     Key? key,
@@ -32,7 +31,7 @@ class BoxedContainer extends StatelessWidget with GetItMixin {
     this.borderRadius,
   }) : super(key: key);
 
-  //  Container-specific variables.
+  // [Container]-specific variables.
   final AlignmentGeometry? alignment;
   final Widget? child;
   final Clip clipBehavior;
@@ -47,36 +46,21 @@ class BoxedContainer extends StatelessWidget with GetItMixin {
   final AlignmentGeometry? transformAlignment;
   final double? width;
 
-  //  _BoxedContainer-specific variables.
+  // [BoxedContainer]-specific variables.
   final Color? borderColor;
   final double? borderRadius;
 
   @override
   Widget build(BuildContext context) {
-    //  Watch for changes to SettingsService, specifically
-    //  SettingsService.settingsData.
-    // bool drawLayoutBounds =
-    //     watchOnly((SettingsServiceOne m) => m.settingsData.drawLayoutBounds);
-    // SettingsServiceSevenImplementation<SettingsDataSeven> test2 = SettingsServiceSevenImplementation<SettingsDataSeven>(SettingsDataSeven());
-    bool drawLayoutBounds =
-        watch<SettingsService, Settings>(
-          // target: test2,
-          // instanceName: 'value',
-        ).drawLayoutBounds;
-    // bool drawLayoutBounds =
-    // watchOnly((SettingsServiceEight m) => m.value.drawLayoutBounds);
-    // print('BoxedContainer, $drawLayoutBounds');
-
-    // print(drawLayoutBounds);
-    // print(test2);
-
-    // bool drawLayoutBounds = true;
+    //  Watch for changes to [SettingsService], specifically
+    //  [SettingsService.value], instigated by the [notifyListeners] method
+    //  bound to [ChangeNotifier].
+    bool drawLayoutBounds = watch<SettingsService, Settings>().drawLayoutBounds;
 
     return Container(
       alignment: alignment,
       child: child,
       clipBehavior: clipBehavior,
-      // color: color,
       constraints: constraints,
       decoration: decoration ??
           BoxDecoration(
