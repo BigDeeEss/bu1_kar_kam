8d70aaef214f7ed8747d6435288be65a58b9c319
Damon Swatton on INSPIRON <damon.swatton@gmail.com>
2023-03-14 20:19:42 +0000

    Add back in get_it_service.dart, settings.dart and settings_service.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 
 // Import project-specific files.
 import 'package:kar_kam/kar_kam.dart';
+import 'package:kar_kam/lib/get_it_service.dart';
+import 'package:kar_kam/settings.dart';
+import 'package:kar_kam/settings_service.dart';
 
 /// App start point.
 void main() {
+  // Use [GetItService] as the single point of access to GetIt to
+  // register an instance of [SettingsService].
+  GetItService.register<SettingsService>(SettingsService(Settings()));
+
   // Run the app.
   runApp(const KarKam());
 }
