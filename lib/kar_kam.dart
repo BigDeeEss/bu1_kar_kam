9f0deac4d7bda9aadd1b98329d4ddf657e252d71
Damon Swatton on INSPIRON <damon.swatton@gmail.com>
2023-03-14 20:25:34 +0000

    Forgot to add files.
    Add back in get_it_service.dart, settings.dart and settings_service.dart.

---
+++ lib/kar_kam.dart
@@ -0,0 +1,52 @@
+// Import flutter packages.
+import 'package:flutter/material.dart';
+
+// Import project-specific files.
+import 'package:kar_kam/lib/get_it_service.dart';
+
+/// KarKam is the root widget of this application.
+///
+/// KarKam is just a StatelessWidget wrapper for an instance of FutureBuilder.
+/// FutureBuilder waits for the async process of loading saved app settings.
+class KarKam extends StatelessWidget {
+  const KarKam({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'KarKam Application',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Material(
+        child: FutureBuilder(
+          future: GetItService.allReady(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              // Present a progress indicator when snapshot has no data.
+              // Case where async load of app settings is still in progress.
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Text('Initialising Kar Kam...',),
+                  SizedBox(height: 16,),
+                  CircularProgressIndicator(),
+                ],
+              );
+            } else {
+              // For the 'has data' case continue with building BasePage.
+              return const MaterialApp(
+                title: '_KarKam',
+                // BasePage invokes a generic page layout so that a similar
+                // UI is presented for each page (route).
+                home: Text('Kar Kam'),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/settings.dart
@@ -0,0 +1,76 @@
+// Import flutter packages.
+import 'package:flutter/material.dart';
+
+/// Stores app settings.
+class Settings {
+  /// A scale factor which is applied to [appBarHeight] in order to calculate
+  /// the [BottomAppBar] height in [BasePage] class.
+  double appBarHeightScaleFactor = 1.0;
+
+  /// The anchor location that determines button placement in [ButtonArray].
+  // static Alignment buttonAlignment = Alignment.bottomLeft;
+  // static Alignment buttonAlignment = Alignment.bottomRight;
+  Alignment buttonAlignment = Alignment.topLeft;
+
+  /// The button axis in [ButtonArray].
+  Axis buttonAxis = Axis.horizontal;
+
+  /// Main axis padding between buttons in [ButtonArray].
+  double buttonPaddingMainAxis = 15.0;
+
+  /// An alternative main axis padding between buttons in [ButtonArray].
+  double buttonPaddingMainAxisAlt = 12.5;
+
+  /// Button radius in [Button] class.
+  double buttonRadius = 28.0;
+
+  /// Whether [BoxedContainer] draws bounding boxes or not.
+  bool drawLayoutBounds = true;
+
+  /// Whether fade effect in SettingsPageListTile is active or not.
+  bool settingsPageListTileFadeEffect = true;
+
+  /// Defines the icon radius in Button.
+  double settingsPageListTileIconSize = 25.0;
+
+  /// Toggles [buttonAxis].
+  void toggleButtonAxis() => buttonAxis = flipAxis(buttonAxis);
+
+  /// Toggles [drawLayoutBounds].
+  void toggleDrawLayoutBounds() => drawLayoutBounds = !drawLayoutBounds;
+
+  /// Toggles [settingsPageListTileFadeEffect].
+  void toggleSettingsPageListTileFadeEffect() =>
+      settingsPageListTileFadeEffect = !settingsPageListTileFadeEffect;
+
+  // For diagnostics.
+  void printThis() {
+    print('Settings, buttonAxis = ${buttonAxis}');
+    print('Settings, drawLayoutBounds = ${drawLayoutBounds}');
+    print(
+        'Settings, settingsPageListTileFadeEffect = ${settingsPageListTileFadeEffect}');
+  }
+
+  /// Updates this using string to determine which field is set to newValue.
+  Settings change({
+    required String identifier,
+    var newValue,
+  }) {
+    // Define a map that converts [string] to a class method.
+    // ToDo: add functionality for other fields in Settings class.
+    Map<String, Function> map = {
+      'appBarHeightScaleFactor': (double newValue) =>
+          appBarHeightScaleFactor = newValue,
+      'buttonAxis': (newValue) => toggleButtonAxis(),
+      'drawLayoutBounds': (newValue) => toggleDrawLayoutBounds(),
+      'settingsPageListTileFadeEffect': (newValue) =>
+          toggleSettingsPageListTileFadeEffect(),
+    };
+
+    // Call the function determined from [map] and update relevant field.
+    map[identifier]?.call(newValue);
+
+    // Return this instance of [Settings].
+    return this;
+  }
+}
