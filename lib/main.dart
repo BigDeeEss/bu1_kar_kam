59e0f8e52f7e1ae78e3f9a18419d530da09513c1
Damon Swatton on INSPIRON <damon.swatton@gmail.com>
2023-03-14 20:08:04 +0000

    Add back in a bare bones KarKam widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,11 @@
-//  Import flutter packages.
+// Import flutter packages.
 import 'package:flutter/material.dart';
 
-//  Import project-specific files.
-// import 'package:dash_cam_app/base_page.dart';
-// import 'package:dash_cam_app/page_specs.dart';
+// Import project-specific files.
+import 'package:kar_kam/kar_kam.dart';
 
-//  App start point.
+/// App start point.
 void main() {
-  runApp(const _KarKam());
-}
-
-/// [_KarKam] is the root widget of this project.
-class _KarKam extends StatelessWidget {
-  const _KarKam({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '_KarKam',
-
-      //  BasePage invokes a generic page layout: a similar UI is
-      //  presented for each page(route).
-      home: Container(),
-      // home: BasePage(
-      //   pageSpec: settingsPage,
-      // ),
-    );
-  }
+  // Run the app.
+  runApp(const KarKam());
 }
